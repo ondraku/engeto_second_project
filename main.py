2a44f4bb115066893d12afe99b7ee9dfc6bc8b3f
ondraku <ondra.kukla@gmail.com>
2021-05-12 13:35:46 +0200

    First draft.

--- main.py
+++ main.py
@@ -1,13 +1,85 @@
 import random
 
-ODDELOVAC = "=" * 100
+SEPARATOR = "=" * 100
 
 def welcome():
     print("Hi there! Welcome to the Bulls & Cows game!")
-    print(ODDELOVAC)
+    print(SEPARATOR)
     print("I've generated a random 4 digit number for you. Can you guess it?\n"
           "Let's play the game!")
-    print(ODDELOVAC)
+    print(SEPARATOR)
+
     return()
 
+random_number = []
+
+def number_generator():
+    while len(random_number) != 4:
+        random_number.append(random.randrange(0, 10))
+        if len(random_number) == 4 and len(set(random_number)) < 4:
+            random_number.clear()
+
+    return()
+
+bulls = 0
+cows = 0
+tries = 0
+remaining_tries = 15
+
+def game():
+    global bulls, cows, tries, remaining_tries
+
+    while bulls != 4 and remaining_tries > 0:
+        players_input = input("Please enter your guess (must be a 4 digit number): ")
+        players_guess = [int(number) for number in str(players_input)]
+        tries += 1
+        remaining_tries -= 1
+
+        if len(players_guess) != 4:
+            print("You must enter a 4 digit number. You have wasted 1 try!")
+            print(f"Remaining tries: {remaining_tries}")
+            print(SEPARATOR)
+
+        elif remaining_tries == 0:
+            print("GAME OVER! Better luck next time!")
+            game_over = "".join(map(str, random_number))
+            print(f"The secret number was: {game_over}")
+            continue
+
+
+        for index, value in enumerate(players_guess):
+            for position, num in enumerate(random_number):
+                if index == position and value == num:
+                    bulls += 1
+
+                elif index != position and value == num:
+                    cows += 1
+
+        print(f"Bulls: {bulls}")
+        print(f"Cows: {cows}")
+        print(f"Remaining tries: {remaining_tries}")
+        print(SEPARATOR)
+
+
+        if remaining_tries == 0:
+            print("GAME OVER! Better luck next time!")
+            game_over = "".join(map(str, random_number))
+            print(f"The secret number was: {game_over}")
+
+        elif bulls != 4:
+            bulls = 0
+            cows = 0
+
+        else:
+            bulls = 4
+            cows = 0
+
+            if tries < 8:
+                print(f"Congratulations, you WON!!! Number of tries: {tries}")
+            elif tries >= 8 and tries < 15:
+                print(f"Congratulations, you WON! But it could be better! Number of tries: {tries}")
+
+
 welcome()
+number_generator()
+game()
